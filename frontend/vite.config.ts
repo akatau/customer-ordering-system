740fe7556b17a0de5afbb55eb6bd43dccc5e9551
ali Khamis <ali.120230129@ejust.edu.eg>
2026-05-18 03:39:43 +0300

    fix last

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,7 +18,9 @@ export default defineConfig({
     },
   },
   server: {
-    port: 5173,
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
